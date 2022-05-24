b5913f762cccaba2af467132cc5ae9bf000a5d1b
britneyart80 <britneychen@Britneys-MBP.lan>
2022-05-24 15:35:11 -0700

    images for code experience

--- my-app/src/utils/experiences/codeExperience.js
+++ my-app/src/utils/experiences/codeExperience.js
@@ -17,6 +17,38 @@ const codeExperiences = [
     "experiences/asics.gif",
     "/code/asics"
   ),
+  new Experience(
+    "Tetris",
+    new Date(2021, 6),
+    new Date(2021, 11),
+    "game of tetris built in pygame",
+    "experiences/tetris.gif",
+    "/code/tetris"
+  ),
+  new Experience(
+    "Beyond the curve",
+    new Date(2021, 6),
+    new Date(2021, 11),
+    "website I made for tutoring company",
+    "experiences/btc.gif",
+    "/code/beyond-the-curve"
+  ),
+  new Experience(
+    "Tic Tac Toe",
+    new Date(2021, 6),
+    new Date(2021, 11),
+    "Game of tic tac toe hosted on heroku",
+    "experiences/ttt.gif",
+    "/code/tic-tac-toe"
+  ),
+  new Experience(
+    "Prepper",
+    new Date(2021, 6),
+    new Date(2021, 11),
+    "HAHHAHAHAHAHAH",
+    "experiences/prepper.gif",
+    "/code/prepper"
+  ),
 ];
 
 export default codeExperiences;
